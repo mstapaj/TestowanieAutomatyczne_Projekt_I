46a2f33c2192407b2727a0dd866f3246e38de4af
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-11-16 11:33:44 +0100

    Dodanie testów get_details

---
+++ tests/test_school_subject.py
@@ -0,0 +1,14 @@
+import unittest
+from assertpy import *
+
+
+class test_SchoolSubject(unittest.TestCase):
+    def setUp(self):
+        self.temp = SchoolSubject('Polski')
+
+    # Testy get_details
+    def test_get_details(self):
+        assert_that(self.temp.get_details()).is_equal_to({
+            'name': 'Polski',
+            'grades': []
+        })
