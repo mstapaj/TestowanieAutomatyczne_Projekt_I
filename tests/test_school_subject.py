818b811a2847a6e7d5ab099ed0743756a4b2503a
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-11-16 11:46:25 +0100

    Dodanie testów edit_subject

--- tests/test_school_subject.py
+++ tests/test_school_subject.py
@@ -13,3 +13,37 @@ class test_SchoolSubject(unittest.TestCase):
             'name': 'Polski',
             'grades': []
         })
+
+    # Testy edit_subject
+    def test_edit_subject_Matematyka(self):
+        assert_that(self.temp.edit_subject('Matematyka')).is_equal_to({
+            'name': 'Matematyka',
+            'grades': []
+        })
+
+    def test_edit_subject_int(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(25)
+
+    def test_edit_subject_float(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(2.5)
+
+    def test_edit_subject_negative_int(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(-10)
+
+    def test_edit_subject_negative_float(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(-3.2)
+
+    def test_edit_subject_object(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with({})
+
+    def test_edit_subject_array(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with([])
+
+    def test_edit_subject_none(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(None)
+
+    def test_edit_subject_true(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(True)
+
+    def test_edit_subject_false(self):
+        assert_that(self.temp.edit_subject).raises(Exception).when_called_with(False)
