9b379a49588f246f1329d41ff5227e0e9ac12399
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-11-20 20:33:14 +0100

    Dodanie edit_student i zmiana konfiguracji CI

--- src/student.py
+++ src/student.py
@@ -12,3 +12,22 @@ class Student:
             'lastname': self.lastname,
             'age': self.age
         }
+
+    def edit_student(self, firstname=None, lastname=None, age=None):
+        if isinstance(firstname, str):
+            self.firstname = firstname
+        elif firstname != None:
+            raise Exception('Błedny typ danych w imieniu')
+        if isinstance(lastname, str):
+            self.lastname = lastname
+        elif lastname != None:
+            raise Exception('Błedny typ danych w nazwisku')
+        if isinstance(age, int) and str(age) != 'True' and str(age) != 'False' and 0 < age:
+            self.age = age
+        elif age != None:
+            raise Exception('Błedny typ danych w wieku')
+        return {
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'age': self.age
+        }
