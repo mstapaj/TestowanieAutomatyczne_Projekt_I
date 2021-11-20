ea7166d3fa1d92bf44fa0372dd6a000184184991
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-11-20 20:10:43 +0100

    Dodanie testów get_details do studenta

---
+++ tests/test_student.py
@@ -0,0 +1,13 @@
+import unittest
+from hamcrest import *
+
+class test_Student(unittest.TestCase):
+    def setUp(self):
+        self.temp = Student('Jan', 'Kowalski', 12)
+
+    def test_get_details(self):
+        assert_that(self.temp.get_details(), equal_to({
+            'firstname': 'Jan',
+            'lastname': 'Kowalski',
+            'age': 12
+        }))
