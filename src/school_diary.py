1b5a777a5cd8b005ea19feb36e28a7f75a4ab31e
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-12-03 13:44:59 +0100

    Dodanie testów w doctest do add_remark_to_student

--- src/school_diary.py
+++ src/school_diary.py
@@ -331,6 +331,18 @@ class SchoolDiary:
             return self.students[student_id - 1].average_of_student()
 
     def add_remark_to_student(self, student_id, text):
+        """
+        >>> c=SchoolDiary()
+        >>> c.add_student('Jan','Kowalski',12)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}]
+        >>> c.add_student('Ola','Kot',8)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}, {'firstname': 'Ola', 'lastname': 'Kot', 'age': 8}]
+        >>> c.add_remark_to_student(1,'Uwaga 1')
+        ['Uwaga 1']
+        >>> c.add_remark_to_student(1,'Uwaga 2')
+        ['Uwaga 1', 'Uwaga 2']
+        >>> c=None
+        """
         if check_student_id(student_id, self):
             if check_remark_text(text):
                 return self.students[student_id - 1].add_remark(text)
