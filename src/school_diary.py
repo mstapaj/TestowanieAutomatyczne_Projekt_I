3451372cfc2ec30212add20749ed82d965773c7a
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-12-03 13:23:33 +0100

    Dodanie testów w doctest do delete_student

--- src/school_diary.py
+++ src/school_diary.py
@@ -82,6 +82,18 @@ class SchoolDiary:
             return self.students[student_id - 1].edit_student(temp['firstname'], temp['lastname'], temp['age'])
 
     def delete_student(self, student_id):
+        """
+        >>> c=SchoolDiary()
+        >>> c.add_student('Jan','Kowalski',12)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}]
+        >>> c.add_student('Ola','Kot',8)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}, {'firstname': 'Ola', 'lastname': 'Kot', 'age': 8}]
+        >>> c.delete_student(2)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}]
+        >>> c.delete_student(1)
+        []
+        >>> c=None
+        """
         if check_student_id(student_id, self):
             self.students.pop(student_id - 1)
             result = []
