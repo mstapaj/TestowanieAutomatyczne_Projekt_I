134f2fe992b443bbca87d341b88875f285696e0e
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-12-03 13:36:26 +0100

    Dodanie testów w doctest do average_of_student

--- src/school_diary.py
+++ src/school_diary.py
@@ -307,6 +307,26 @@ class SchoolDiary:
                 return self.students[student_id - 1].average_of_subject(subject_id)
 
     def average_of_student(self, student_id):
+        """
+        >>> c=SchoolDiary()
+        >>> c.add_student('Jan','Kowalski',12)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}]
+        >>> c.add_student('Ola','Kot',8)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}, {'firstname': 'Ola', 'lastname': 'Kot', 'age': 8}]
+        >>> c.add_subject_to_student(1,'Matematyka')
+        {'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12, 'subjects': ['Matematyka']}
+        >>> c.add_subject_to_student(1,'Fizyka')
+        {'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12, 'subjects': ['Matematyka', 'Fizyka']}
+        >>> c.add_grade_in_student_in_subject(1,1,3)
+        {'Matematyka': [3], 'Fizyka': []}
+        >>> c.add_grade_in_student_in_subject(1,1,6)
+        {'Matematyka': [3, 6], 'Fizyka': []}
+        >>> c.add_grade_in_student_in_subject(1,2,5)
+        {'Matematyka': [3, 6], 'Fizyka': [5]}
+        >>> c.average_of_student(1)
+        4.5
+        >>> c=None
+        """
         if check_student_id(student_id, self):
             return self.students[student_id - 1].average_of_student()
 
