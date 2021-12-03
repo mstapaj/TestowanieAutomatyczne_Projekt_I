b9327dd218d85a4548b8d462a7a691037398dde1
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-12-03 10:14:44 +0100

    Dodanie testów check_lastname w pytest

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -4,6 +4,7 @@ from src.checks.checks_student import check_remark_text
 from src.checks.checks_school_subject import check_subject_name, check_grade
 
 
+# Testy check_firstname
 def test_check_firstname():
     assert check_firstname('Jan') == True
 
@@ -59,3 +60,61 @@ def test_check_firstname_negative_int():
 def test_check_firstname_negative_float():
     with pytest.raises(TypeError):
         check_firstname(-7.12)
+
+
+# Testy check_lastname
+def test_check_lastname():
+    assert check_lastname('Kowalski') == True
+
+
+def test_check_lastname_2():
+    assert check_lastname('Nowak') == True
+
+
+def test_check_lastname_3():
+    assert check_lastname('abcdefghijklmnopqrsteuwyxz') == True
+
+
+def test_check_lastname_none():
+    with pytest.raises(TypeError):
+        check_lastname(None)
+
+
+def test_check_lastname_object():
+    with pytest.raises(TypeError):
+        check_lastname([])
+
+
+def test_check_lastname_array():
+    with pytest.raises(TypeError):
+        check_lastname({})
+
+
+def test_check_lastname_true():
+    with pytest.raises(TypeError):
+        check_lastname(True)
+
+
+def test_check_lastname_false():
+    with pytest.raises(TypeError):
+        check_lastname(False)
+
+
+def test_check_lastname_int():
+    with pytest.raises(TypeError):
+        check_lastname(12)
+
+
+def test_check_lastname_float():
+    with pytest.raises(TypeError):
+        check_lastname(4.12)
+
+
+def test_check_lastname_negative_int():
+    with pytest.raises(TypeError):
+        check_lastname(-3)
+
+
+def test_check_lastname_negative_float():
+    with pytest.raises(TypeError):
+        check_lastname(-7.12)
