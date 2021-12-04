d8ff4837ee62af0af1a95dda66016fc499424166
Mateusz Stapaj <=mtxstapaj@gmail.com>
2021-12-04 13:07:20 +0100

    Dodanie testów w doctest do delete_remark_in_student

--- src/school_diary.py
+++ src/school_diary.py
@@ -388,6 +388,22 @@ class SchoolDiary:
                     return self.students[student_id - 1].edit_remark(remark_id, text)
 
     def delete_remark_in_student(self, student_id, remark_id):
+        """
+        >>> c=SchoolDiary()
+        >>> c.add_student('Jan','Kowalski',12)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}]
+        >>> c.add_student('Ola','Kot',8)
+        [{'firstname': 'Jan', 'lastname': 'Kowalski', 'age': 12}, {'firstname': 'Ola', 'lastname': 'Kot', 'age': 8}]
+        >>> c.add_remark_to_student(1,'Uwaga 1')
+        ['Uwaga 1']
+        >>> c.add_remark_to_student(1,'Uwaga 2')
+        ['Uwaga 1', 'Uwaga 2']
+        >>> c.delete_remark_in_student(1,2)
+        ['Uwaga 1']
+        >>> c.delete_remark_in_student(1,1)
+        []
+        >>> c=None
+        """
         if check_student_id(student_id, self):
             if check_remark_id(student_id, remark_id, self):
                 return self.students[student_id - 1].delete_remark(remark_id)
